229c58415fcf897a5412af6ebc9996ea18a95d09
tno1 <nnordendorf@gmail.com>
2021-12-15 19:29:27 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BitBurner
+Tno BitBurner Stats
--- import.js
+++ import.js
@@ -13,7 +13,7 @@ export async function main(ns) {
     ns.tprint('Hey! Thank you for downloading the BitBurner Scripts.');
     ns.tprint(`You've installed these in the ${config.folder} directory.`);
     ns.tprint(
-      `A good place to start is running \`run /${config.folder}/hax.js\``
+      `A good place to start is running \`run /${config.folder}/hack.js\``
     );
   } else {
     ns.tprint(
