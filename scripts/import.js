36cb52e7fcdebea532a08ce884ed853c9d1a4c4d
tno1 <nnordendorf@gmail.com>
2021-12-16 15:24:58 +0100

    Update

--- scripts/import.js
+++ scripts/import.js
@@ -1,5 +1,5 @@
 let config = {
-  folder: 'scripts',
+  folder: '',
   rootUrl: 'https://raw.githubusercontent.com/tno1/bitburner/main/',
   serverPrefix: 'TnoPrefix',
 };
@@ -10,11 +10,6 @@ export async function main(ns) {
   let filesImported = await importFiles(ns);
   ns.tprint('='.repeat(20));
   if (filesImported) {
-    ns.tprint('Hey! Thank you for downloading the BitBurner Scripts.');
-    ns.tprint(`You've installed these in the ${config.folder} directory.`);
-    ns.tprint(
-      `A good place to start is running \`run /${config.folder}/hack.js\``
-    );
   } else {
     ns.tprint(
       'You had some issues downloading files, please reach out to the repo maintainer or check your config.'
@@ -29,8 +24,8 @@ async function importFiles(ns) {
   ];
   let filesImported = true;
   for (let file of files) {
-    let remoteFileName = `${config.rootUrl}scripts/${file}`;
-    let result = await ns.wget(remoteFileName, `/${getFolder()}/${file}`);
+    let remoteFileName = `${file}`;
+    let result = await ns.wget(remoteFileName, `${file}`);
     filesImported = filesImported && result;
     ns.tprint(`File: ${file}: ${result ? '✔️' : '❌'}`);
   }
