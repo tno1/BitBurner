ed062417cca82d518fea287a2b5cc05158aec729
tno1 <nnordendorf@gmail.com>
2021-12-16 15:38:39 +0100

    Done

--- scripts/import.js
+++ scripts/import.js
@@ -26,7 +26,7 @@ async function importFiles(ns) {
   let filesImported = true;
   for (let file of files) {
     let remoteFileName = `${config.rootUrl}scripts/${file}`;
-    let result = await ns.wget(remoteFileName, file);
+    let result = await ns.wget(remoteFileName, `/${getFolder()}/${file}`);
     filesImported = filesImported && result;
     ns.tprint(`File: ${file}: ${result ? '✔️' : '❌'}`);
   }
