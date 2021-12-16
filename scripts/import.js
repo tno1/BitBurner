e1f0066aa0999f441a05d32903d568fc98470e3e
tno1 <nnordendorf@gmail.com>
2021-12-16 15:34:32 +0100

    Done

--- scripts/import.js
+++ scripts/import.js
@@ -25,22 +25,12 @@ async function importFiles(ns) {
   ];
   let filesImported = true;
   for (let file of files) {
-    let remoteFileName = `${file}`;
-    let result = await ns.wget(remoteFileName, `${file}`);
+    let remoteFileName = file;
+    let result = await ns.wget(remoteFileName, file);
     filesImported = filesImported && result;
     ns.tprint(`File: ${file}: ${result ? '✔️' : '❌'}`);
   }
   return filesImported;
 }
 
-export function getFolder() {
-  return config.folder;
-}
 
-export function getServerPrefix() {
-  return config.serverPrefix;
-}
-
-export function getHackScript() {
-  return `/${getFolder()}/hack.js`;
-}
